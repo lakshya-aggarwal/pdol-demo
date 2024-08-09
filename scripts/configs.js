0a7f2e86ef9f938d1e8e57f095825f9930441e81
lakshya aggarwal <laggarwal@adobe.com>
2024-08-09 11:32:34 +0530

    updated configs path for AEM

--- scripts/configs.js
+++ scripts/configs.js
@@ -38,6 +38,64 @@ const getConfigForEnvironment = async (environment) => {
     configJSON = await fetch(buildConfigURL(env)).then((res) => res.text());
     window.sessionStorage.setItem(`config:${env}`, configJSON);
   }
+  if (!configJSON) {
+    configJSON = {
+      total: 12,
+      offset: 0,
+      limit: 12,
+      data: [
+        {
+          key: 'commerce-endpoint',
+          value: 'https://catalog-service-sandbox.adobe.io/graphql',
+        },
+        {
+          key: 'commerce-environment-id',
+          value: 'a96ede16-88d0-45ec-b897-3a6bdca25324',
+        },
+        {
+          key: 'commerce-website-code',
+          value: 'wknd',
+        },
+        {
+          key: 'commerce-store-view-code',
+          value: 'wknd',
+        },
+        {
+          key: 'commerce-store-code',
+          value: 'wknd_store',
+        },
+        {
+          key: 'commerce-customer-group',
+          value: 'b6589fc6ab0dc82cf12099d1c2d40ab994e8410c',
+        },
+        {
+          key: 'commerce-x-api-key',
+          value: 'storefront-widgets',
+        },
+        {
+          key: 'commerce-core-endpoint',
+          value: 'https://wknd-commerce.adobedemo.com/graphql',
+        },
+        {
+          key: 'commerce-root-category-id',
+          value: '3',
+        },
+        {
+          key: 'aem-host',
+          value: 'https://publish-p64656-e692852.adobeaemcloud.com/',
+        },
+        {
+          key: 'aem-graphql-endpoint',
+          value: 'aem-demo-assets',
+        },
+        {
+          key: 'adyen-client-key',
+          value: 'test_DQGCHYCTAVA63LJNHCE5TQPU3AEIGRZ2',
+        },
+      ],
+      ':type': 'sheet',
+    };
+  }
   console.log(configJSON);
   return configJSON;
 };
